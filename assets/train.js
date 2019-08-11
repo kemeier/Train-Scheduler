92850ba427d2ced630f08a09e9cf12f41312fdd2
Kelly Meier <kemeier2000@yahoo.com>
2019-08-11 17:19:24 -0500

    new js

--- assets/train.js
+++ assets/train.js
@@ -1,20 +1,21 @@
-$(document).ready(function() {
- 
-var firebaseConfig = {
-    apiKey: "AIzaSyDUGhFAzqegpcdqxI7WSHTms3YFy1vsi2g",
-    authDomain: "trainscheduler-1da25.firebaseapp.com",
-    databaseURL: "https://trainscheduler-1da25.firebaseio.com",
-    projectId: "trainscheduler-1da25",
-    storageBucket: "",
-    messagingSenderId: "376469405618",
-    appId: "1:376469405618:web:da262c5d9b43addf"
-  };
 
-  firebase.initializeApp(firebaseConfig);
+    $( document ).ready(function() {
+    
+    var trainName = "";
+    var destination = "";
+    var trainTime = 0;
+    var frequency = 0;
 
-  $("#button").click(function() {
-    console.log("clicked");
-})
+       
+    
+    $("#button").on("click", function(event) {
+        alert("clicked");
 
+        var firebaseRef = firebase.database().ref();
+
+        firebaseRef.child("Text").set("Some Value");
+    
+    })
+    
 });
 
