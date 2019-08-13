b3dd32ed1331431ca22e33465b69ad14d6b2bc40
Kelly Meier <kemeier2000@yahoo.com>
2019-08-13 13:51:29 -0500

    finished button and added newTrain variable

--- assets/train.js
+++ assets/train.js
@@ -1,21 +1,27 @@
 
-    $( document ).ready(function() {
-    
-    var trainName = "";
-    var destination = "";
-    var trainTime = 0;
-    var frequency = 0;
 
-       
-    
+    firebase.initializeApp(firebaseConfig);
+
+    var train = firebase.database();
+
+        
     $("#button").on("click", function(event) {
         alert("clicked");
+        event.preventDefault();
 
-        var firebaseRef = firebase.database().ref();
+        var trainName = $("#train-name").val().trim();
+        var destination = $("#destination").val().trim();
+        var trainTime = moment($("#train-Time").val().trim(), "hh:mm").subtract(1, "years").format("X");
+        var frequency = $("#frequency").val().trim();
 
-        firebaseRef.child("Text").set("Some Value");
+        var currentTime = moment();
+	    console.log("CURRENT TIME: " +  moment(currentTime).format("hh:mm"));
+
+    var newTrain = {
+        name: trainName,
+        whereGoing: destination,
+        trainComing: trainTime,
+        howOften: frequency
+    };
+
     
-    })
-    
-});
-
